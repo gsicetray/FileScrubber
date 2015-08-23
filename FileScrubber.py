3d99414b3dcd43b46188dc2780ab98ebb93fd456
Grant <gsicetray@gmail.com>
2015-08-23 15:36:25 -0400

    Initial Working Program
    created at Luck Bros august 23

---
+++ FileScrubber.py
@@ -0,0 +1,5 @@
+""" This is the FileScrubber ServiceNow Utility, for analyzing the horrible data sent in to CMDB
+Hope it works out
+"""
+welcome_message = "Welcome to FileScrubber, brought to you by Bitseve Software"
+print welcome_message
